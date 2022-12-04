60ec1e586feb226e80f674955799a2ffd2bc39da
chenshenchao <chenshenchao@outlook.com>
2022-12-04 18:54:38 +0800

    save

--- fledgeos/fosdemo/src/main.rs
+++ fledgeos/fosdemo/src/main.rs
@@ -7,29 +7,34 @@ mod vga;
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
-use x86_64::instructions::{hlt};
-use vga::{ Cursor };
+use vga::{Color, Cursor};
+use x86_64::instructions::hlt;
 
 #[panic_handler]
 #[no_mangle]
 pub fn panic(info: &PanicInfo) -> ! {
-  let mut cursor = Cursor::new();
-  for _ in 0..(80*25) {
-    cursor.print(b" ");
-  }
-  cursor.to_begin();
-  write!(cursor, "{}", info).unwrap();
+    let mut cursor = Cursor::new(Color::White, Color::Red);
+    for _ in 0..(80 * 25) {
+        cursor.print(b" ");
+    }
+    cursor.to_begin();
+    write!(cursor, "{}", info).unwrap();
 
-  loop {
-    hlt();
-  }
+    loop {
+        hlt();
+    }
 }
 
+//
 #[lang = "eh_personality"]
 #[no_mangle]
-pub extern "C" fn eh_personality() { }
+pub extern "C" fn eh_personality() {}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  panic!("help!");
+    main();
+}
+
+fn main() -> ! {
+    panic!("help!");
 }
--- fledgeos/fosdemo/src/vga.rs
+++ fledgeos/fosdemo/src/vga.rs
@@ -4,69 +4,68 @@ use core::fmt;
 #[derive(Copy, Clone)]
 #[repr(u8)]
 pub enum Color {
-  Black = 0x0,
-  White = 0xF,
-  Blue = 0x1,
-  BrightBlue = 0x9,
-  Green = 0x2,
-  BrightGreen = 0xA,
-  Cyan = 0x3,
-  BrightCyan = 0xB,
-  Red = 0x4,
-  BrightRed = 0xC,
-  Magenta = 0x5,
-  BrightMagenta = 0xD,
-  Brown = 0x6,
-  Yellow = 0xE,
-  Gray = 0x7,
-  DarkGray = 0x8
+    Black = 0x0,
+    White = 0xF,
+    Blue = 0x1,
+    BrightBlue = 0x9,
+    Green = 0x2,
+    BrightGreen = 0xA,
+    Cyan = 0x3,
+    BrightCyan = 0xB,
+    Red = 0x4,
+    BrightRed = 0xC,
+    Magenta = 0x5,
+    BrightMagenta = 0xD,
+    Brown = 0x6,
+    Yellow = 0xE,
+    Gray = 0x7,
+    DarkGray = 0x8,
 }
 
 pub struct Cursor {
-  position: isize,
-  foreground: Color,
-  background: Color,
+    position: isize,
+    foreground: Color,
+    background: Color,
 }
 
 impl Cursor {
-    pub fn new() -> Cursor {
+    pub fn new(foreground: Color, background: Color) -> Cursor {
         Cursor {
             position: 0,
-            foreground: Color::White,
-            background: Color::Red,
+            foreground: foreground,
+            background: background,
         }
     }
 
-  pub fn color(&self) -> u8 {
-    let fg = self.foreground as u8;
-    let bg = (self.background as u8) << 4;
-    fg | bg
-  }
-
-  pub fn print(&mut self, text: &[u8]) {
-    let color = self.color();
-
-    let framebuffer = 0xb8000 as *mut u8;
-
-    for &character in text {
-      unsafe {
-        framebuffer.offset(self.position)
-            .write_volatile(character);
-        framebuffer.offset(self.position + 1)
-            .write_volatile(color);
-      }
-      self.position += 2;
+    pub fn color(&self) -> u8 {
+        let fg = self.foreground as u8;
+        let bg = (self.background as u8) << 4;
+        fg | bg
     }
-  }
 
-  pub fn to_begin(&mut self) {
-    self.position = 0;
-  }
+    pub fn print(&mut self, text: &[u8]) {
+        let color = self.color();
+
+        let framebuffer = 0xb8000 as *mut u8;
+
+        for &character in text {
+            unsafe {
+                framebuffer.offset(self.position).write_volatile(character);
+                framebuffer.offset(self.position + 1).write_volatile(color);
+            }
+            self.position += 2;
+        }
+    }
+
+    pub fn to_begin(&mut self) {
+        self.position = 0;
+    }
 }
 
+// 实现格式化写，定向输出到屏幕。
 impl fmt::Write for Cursor {
-  fn write_str(&mut self, s: &str) -> fmt::Result {
-    self.print(s.as_bytes());
-    Ok(())
-  }
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.print(s.as_bytes());
+        Ok(())
+    }
 }
